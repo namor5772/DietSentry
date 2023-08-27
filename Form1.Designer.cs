e3a1887e2f5e25db524558ba59359194b0c0977a
roman groblicki <romangroblicki@proton.me>
2023-08-28 07:40:24 +1000

    Git Test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Name = "MainForm";
-            Text = "Diet Sentry";
+            Text = "Diet Sentry Git Test";
             ResumeLayout(false);
         }
 
